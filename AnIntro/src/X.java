8f8cf0c654de4ca01f60cc8f2c43109a0b0f494a
Manoj Palat <manoj.palat@in.ibm.com>
2024-10-08 05:50:15 +0530

    resetting X

--- AnIntro/src/X.java
+++ AnIntro/src/X.java
@@ -1,12 +1,4 @@
 public class X {
 
-	private static int sum(int a, int b) {
-		return a + b;
-	}
-
-	public static void main(String[] args) {
-//		int sum = X.sum(10, 2);
-//		System.out.println(sum);
-	}
 }
 
