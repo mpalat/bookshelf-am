253225fbc20d2fa20938982b8e575eda40f87f21
Manoj Palat <manoj.palat@in.ibm.com>
2024-10-08 05:46:58 +0530

    Adding the intro product

---
+++ AnIntro/src/X.java
@@ -0,0 +1,12 @@
+public class X {
+
+	private static int sum(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+//		int sum = X.sum(10, 2);
+//		System.out.println(sum);
+	}
+}
+
